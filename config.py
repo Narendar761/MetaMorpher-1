1f18cefc79ee638529a18759a34e05276586e207
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2024-09-13 14:18:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = int(os.environ.get("API_ID", "10811400"))
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6826963075:AAH82I95CN08QWChY2LzHNZJ4bAslC6WcVs")
-ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", '')) 
 FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
 FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
 CAPTION = os.environ.get("CAPTION", "")
@@ -18,7 +18,7 @@ DOWNLOAD_LOCATION = "./DOWNLOADS"
 group = environ.get('GROUP', '-1002173560131')
 GROUP = int(group) if group and id_pattern.search(group) else None
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", ''))
 
 
 
